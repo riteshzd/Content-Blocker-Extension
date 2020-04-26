b84d91a2857de0494fb57126399d680aa161162f
Ritesh <riteshdash305@gmail.com>
2020-04-26 21:23:56 +0530

    Added a dynamic Regular Expression

--- contentBlocker.js
+++ contentBlocker.js
@@ -1,12 +1,15 @@
+const reqWord = 'china'
+
 contentBlocker(document.body)
 
 function contentBlocker(element) {
     if(element.hasChildNodes()){
         element.childNodes.forEach(contentBlocker)
     } else if(element.nodeType === Text.TEXT_NODE){
-                if(element.textContent.match(/china/gi)){
+                var dynamicRegExp = new RegExp(reqWord,"gi")
+                if(element.textContent.match(dynamicRegExp)){
                     const newElement = document.createElement("span")
-                    newElement.innerHTML = element.textContent.replace(/(china)/gi,'<span class="blockerBody">$1</span>')
+                    newElement.innerHTML = element.textContent.replace(dynamicRegExp,'<span class="blockerBody">$1</span>')
                     element.replaceWith(newElement)
                 }    
             }   
