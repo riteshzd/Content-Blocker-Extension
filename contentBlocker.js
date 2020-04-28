e6efa0421291ae4b4d381a6a31e55726682d61b0
Ritesh <riteshdash305@gmail.com>
2020-04-28 18:23:51 +0530

    added popup folder and defined style of popup

--- contentBlocker.js
+++ contentBlocker.js
@@ -1,16 +1,20 @@
-const reqWord = 'china'
-
+const wordList = ['china','wuhan','dead','virus']
 contentBlocker(document.body)
 
 function contentBlocker(element) {
     if(element.hasChildNodes()){
         element.childNodes.forEach(contentBlocker)
     } else if(element.nodeType === Text.TEXT_NODE){
-                var dynamicRegExp = new RegExp(reqWord,"gi")
-                if(element.textContent.match(dynamicRegExp)){
-                    const newElement = document.createElement("span")
-                    newElement.innerHTML = element.textContent.replace(dynamicRegExp,'<span class="blockerBody">$1</span>')
-                    element.replaceWith(newElement)
-                }    
+
+                for(var i=0;i<wordList.length;i++){
+                    var dynamicRegExp = new RegExp(wordList[i],"gi")
+
+                    if(element.textContent.match(dynamicRegExp)){
+                        const newElement = document.createElement("span")
+                        newElement.innerHTML = element.textContent.replace(dynamicRegExp,'<span class="blockerBody">$1</span>')
+                        element.replaceWith(newElement)
+                    }  
+                }
+                  
             }   
 }
